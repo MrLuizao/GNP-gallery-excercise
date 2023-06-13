81ab289fe0e964c3ad1c8cd9fd2f26f32ff4e80d
Luis VR <luis_vazquez.r@hotmail.com>
2023-06-13 11:49:47 -0600

    project structure

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [MatSidenavModule, MatFormFieldModule, MatSelectModule, MatButtonModule],
+})
+export class HomeComponent {
+
+}
